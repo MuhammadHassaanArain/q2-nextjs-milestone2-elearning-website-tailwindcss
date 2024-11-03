21d72ed52023322372758a93c1aeb846a4849caf
Hassaan Arain <rafatnaz21@gmail.com>
2024-11-03 14:00:09 +0500

    code pushed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,86 @@
-
-function AboutUs() {
+export default function AboutUs() {
   return (
-    <div>AboutUs Page</div>
-  )
+  <div className="flex flex-col items-center bg-gray-100 py-10">
+      <h2 className="text-blue-600 text-3xl font-bold py-4">About LearnHub</h2>
+      <main className="text-gray-900 w-4/5  rounded border border-gray-200 p-4 shadow-xl">
+      
+      <section>
+      
+        <p>
+          Welcome to Learn Hub—your trusted platform for high-quality, flexible,
+          and impactful online education. At Learn Hub, we believe that
+          education is the cornerstone of personal and professional growth. Our
+          mission is to make learning accessible to everyone, regardless of
+          location, background, or previous experience, and to provide you with
+          the skills needed to thrive in today’s rapidly changing world.
+        </p>
+      </section>
+      <section>
+        <h3 className="text-blue-600 text-xl pt-4">Our Story</h3>
+        <p>
+          Learn Hub was founded with a simple but powerful vision: to
+          democratize learning by creating a platform where anyone, anywhere,
+          can access top-tier educational content. We started with a handful of
+          courses, but over time, we've expanded to include a diverse range of
+          subjects and specializations. Today, our platform is home to hundreds
+          of expertly designed courses across fields such as technology,
+          business, creative arts, personal development, and more.
+          <br />
+          <br />
+          With acommitment to quality and continuous improvement, we strive to
+          make Learn Hub a place where learners can transform their passions
+          into skills, and their skills into careers.
+        </p>
+      </section>
+      <section>
+        <h3  className="text-blue-600 text-xl pt-4">Our Values</h3>
+        <p>
+          {" "}
+          <span>Accessibility:</span> We believe everyone deserves the chance to
+          learn, which is why we offer courses that are affordable and
+          accessible to a global audience.
+        </p>
+        <p>
+          <span>Quality:</span>Every course on Learn Hub is created by industry
+          professionals and vetted to ensure high educational standards.
+        </p>
+        <p>
+          <span>Flexibility:</span>We understand that life is busy. Our courses
+          are designed to fit into your schedule, allowing you to learn at your
+          own pace.
+        </p>
+        <p>
+          <span>Community</span> Learning is more effective when shared. Our
+          platform fosters a supportive community of learners and instructors
+          who inspire and help one another succeed.
+        </p>
+      </section>
+      <section>
+        <h3  className="text-blue-600 text-xl pt-4">Our Vision</h3>
+        <p>
+          At Learn Hub, our vision is to create a world where learning is
+          continuous, accessible, and enjoyable. We want to empower people
+          everywhere to reach their full potential and achieve their dreams. By
+          offering quality, accessible education, we believe we can make a
+          meaningful impact in people’s lives, enabling them to adapt to new
+          challenges and opportunities.
+        </p>
+      </section>
+      <section>
+        <h3  className="text-blue-600 text-xl pt-4">Join Us on Your Learning Journey</h3>
+        <p>
+          Are you ready to take the next step in your education? Join the Learn
+          Hub community today and unlock your potential. Whether you're starting
+          a new career, upskilling, or simply following your curiosity, we’re
+          here to help you succeed. Let’s grow, learn, and achieve together—one
+          course at a time.
+        </p>
+      </section>
+      <br />
+      <br />
+      <br />
+      <h2 className="text-blue-600 text-2xl py-4">Start your journey with Learn Hub, where learning meets opportunity!</h2>
+    </main>
+  </div>
+  );
 }
-
-export default AboutUs
